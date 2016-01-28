0b822f2e9e3200defedfc9559ee30c2f83e29733
Marius <cmd-m@cmd-m-2.local>
2016-01-29 00:28:57 +0100

    updated enemy states

--- LuMaxMan/EnemyEscapingState.swift
+++ LuMaxMan/EnemyEscapingState.swift
@@ -11,7 +11,21 @@ import GameplayKit
 
 class EnemyEscapingState: GKState {
     
+    // MARK : Properties
+    
+    // The connection to the Enemy being in this state.
     unowned var entity: EnemyEntity
+    
+    // The time the Enemy currently is in this state.
+    //var elapsedTime: NSTimeInterval = 0.0
+    
+    // The MovementComponent associated with the Enemy.
+    var movementComponent: MovementComponent {
+        guard let movementComponent = entity.componentForClass(MovementComponent.self) else {
+            fatalError("An EnemyFollowingState must have a MovementComponent.")
+        }
+        return movementComponent
+    }
     
     
     // MARK: Initializers
@@ -20,6 +34,26 @@ class EnemyEscapingState: GKState {
         self.entity = entity
     }
     
+    override func didEnterWithPreviousState(previousState: GKState?) {
+        super.didEnterWithPreviousState(previousState)
+        
+        // Reset the elapsed time.
+        // elapsedTime = 0.0
+        
+        let movementComponent = self.movementComponent
+        movementComponent.nextTranslation = nil
+    }
+    
+    override func updateWithDeltaTime(seconds: NSTimeInterval) {
+        super.updateWithDeltaTime(seconds)
+        
+        //elapsedTime += seconds
+        
+        // If the Enemy is about to follow LumaxMan, re-enter EnemyFollowingState.
+        if entity.isFollowing {
+            stateMachine?.enterState(EnemyFollowingState.self)
+        }
+    }
     
     override func isValidNextState(stateClass: AnyClass) -> Bool {
         switch stateClass {
@@ -29,5 +63,12 @@ class EnemyEscapingState: GKState {
         default:
             return false
         }
+    }
+    
+    override func willExitWithNextState(nextState: GKState) {
+        super.willExitWithNextState(nextState)
+        
+        let movementComponent = self.movementComponent
+        movementComponent.nextTranslation = nil
     }
 }
